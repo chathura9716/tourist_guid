96f6ac1402cab118c53aabc3a696109f1ef49f93
chathura9716 <lakmalc97516@gmail.com>
2022-10-28 11:36:56 +0530

    contact icon added

--- themes/Bootstrap/views/user/tourist/hotel.blade.php
+++ themes/Bootstrap/views/user/tourist/hotel.blade.php
@@ -104,16 +104,17 @@
 			</div>
 		</div>
 <!--==============================footer=================================-->
-		<footer>
+<footer>
 			<div class="container_12">
 				<div class="grid_12">
 					<div class="socials">
+					<a href="{{route('touristContact')}}" class="fa fa-phone"></a>
 						<a href="#" class="fa fa-facebook"></a>
 						<a href="#" class="fa fa-twitter"></a>
 						<a href="#" class="fa fa-google-plus"></a>
 					</div>
 					<div class="copy">
-						Your Trip (c) 2014 | <a href="#">Privacy Policy</a> | Website Template Designed by TemplateMonster.com
+						Tourist guid (c) 2022 | <a href="#">Privacy Policy</a> | 
 					</div>
 				</div>
 			</div>
